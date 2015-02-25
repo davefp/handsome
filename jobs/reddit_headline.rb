17bb3fdd581e3d027c87975495052b27b8098307
David Underwood <davefp@gmail.com>
2015-02-24 19:08:03 -0500

    Update reddit job to skip the sticky

--- jobs/reddit_headline.rb
+++ jobs/reddit_headline.rb
@@ -1,5 +1,5 @@
 recurring_job('1m') do
-  listing = JSON.parse(Net::HTTP.get(URI('https://www.reddit.com/r/todayilearned.json?limit=1')))
-  title = listing['data']['children'][0]['data']['title']
+  listing = JSON.parse(Net::HTTP.get(URI('https://www.reddit.com/r/todayilearned.json?limit=2')))
+  title = listing['data']['children'][1]['data']['title']
   {reddit_headline: {text: title}}
 end
