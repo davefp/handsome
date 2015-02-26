8303e3c2cfcd2d221efd94161f5ab65cdddb129e
David Underwood <davefp@gmail.com>
2015-02-26 07:38:16 -0500

    add list to default widget and add max width to dashboard

--- dashboards/default.jsx
+++ dashboards/default.jsx
@@ -1,9 +1,9 @@
 React.render(
   <div id="dashboard">
-    <NumberWidget name="number" title="Number" />
+    <NumberWidget name="reddit_score" title="Score" />
     <TextWidget name="reddit_headline" title="Top Reddit Post" width="2" />
-    <ImageWidget name="kitten" title="Kitten" />
     <ListWidget name="top_subreddits" title="Most Popular Subreddits" height="2" />
+    <ImageWidget name="kitten" title="Kitten" />
   </div>,
   document.getElementById('content')
 );
--- jobs/reddit_headline.rb
+++ jobs/reddit_headline.rb
@@ -1,5 +1,9 @@
 recurring_job('1m') do
   listing = JSON.parse(Net::HTTP.get(URI('https://www.reddit.com/r/todayilearned.json?limit=2')))
   title = listing['data']['children'][1]['data']['title']
-  {reddit_headline: {text: title}}
+  score = listing['data']['children'][1]['data']['score']
+  {
+    reddit_headline: {text: title},
+    reddit_score: {number: score}
+  }
 end
