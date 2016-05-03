6f52299162efc01ceafecdc91903b84eeb8f8669
David Underwood <davefp@gmail.com>
2016-05-03 11:34:31 -0400

    quick fix to set redis password to undefined if it's not present

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var redis = require('redis');
 var config = {
   redis_server_host: process.env.REDIS_SERVER_HOST || "127.0.0.1",
   redis_server_port: process.env.REDIS_SERVER_PORT || 6379,
-  redis_server_password: process.env.REDIS_SERVER_PASSWORD || "",
+  redis_server_password: process.env.REDIS_SERVER_PASSWORD || undefined
 };
 
 exports.getRedisClient = function() {
