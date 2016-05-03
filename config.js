0c4194796ad6389d08edf7aee26a9b99d72c453a
Robbie Page <rob@rorpage.com>
2016-05-03 08:43:01 -0400

    addressed feedback

--- config.js
+++ config.js
@@ -1,21 +1,25 @@
 var redis = require('redis');
 
-exports.config = {
-  redis_server_url: "127.0.0.1",
-  redis_server_port: 6379,
+var config = {
+  redis_server_host: process.env.REDIS_SERVER_HOST || "127.0.0.1",
+  redis_server_port: process.env.REDIS_SERVER_PORT || 6379,
   redis_server_requires_auth: false,
-  redis_server_password: ""
+  redis_server_password: process.env.REDIS_SERVER_PASSWORD || "",
 };
 
 exports.getRedisClient = function() {
-  if (this.config.redis_server_url && this.config.redis_server_port) {
+  if (config.redis_server_host && config.redis_server_port) {
     var options = {};
 
-    if (this.config.redis_server_requires_auth && this.config.redis_server_password) {
-      options.password = this.config.redis_server_password;
+    if (config.redis_server_requires_auth) {
+      if (config.redis_server_password) {
+        options.password = config.redis_server_password;
+      } else {
+        throw "Server requires auth but no password was supplied";
+      }
     }
 
-    return redis.createClient(this.config.redis_server_port, this.config.redis_server_url, options);
+    return redis.createClient(config.redis_server_port, config.redis_server_host, options);
   } else {
     return redis.createClient();
   }
