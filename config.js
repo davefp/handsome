e721ba5aedecc28c5c8234d4352bafac0de70dd7
David Underwood <davefp@gmail.com>
2017-08-04 12:16:38 -0400

    add a redis key prefix

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ var storage_options = {
   engine: 'redis',
   port: process.env.REDIS_SERVER_PORT || 6379,
   host: process.env.REDIS_SERVER_HOST || "127.0.0.1",
-  password: process.env.REDIS_SERVER_PASSWORD || undefined
+  password: process.env.REDIS_SERVER_PASSWORD || undefined,
+  prefix: process.env.REDIS_PREFIX || "handsome"
 };
 
 exports.getStorage = function() {
