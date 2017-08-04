161905f2e95e11a7b45bf610c6e65c5fd43a4103
David Underwood <davefp@gmail.com>
2017-08-04 12:37:55 -0400

    remove ttl from storage backend by default

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ var storage_options = {
   port: process.env.REDIS_SERVER_PORT || 6379,
   host: process.env.REDIS_SERVER_HOST || "127.0.0.1",
   password: process.env.REDIS_SERVER_PASSWORD || undefined,
-  prefix: process.env.REDIS_PREFIX || "handsome"
+  prefix: process.env.REDIS_PREFIX || "handsome",
+  ttl: -1
 };
 
 exports.getStorage = function() {
